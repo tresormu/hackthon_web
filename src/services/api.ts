9b18f1b8cdfd030b91ed0523f1ca6ed0f67b5c66
tresor <tresormugisha07@gmail.com>
2026-03-31 21:59:44 +0200

    hosiptal patient validation

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_APP_API_URL || "http://localhost:4000/api",
+  baseURL: import.meta.env.VITE_APP_API_URL ,
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
